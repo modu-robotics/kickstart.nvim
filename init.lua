e40aaf641889159fd0399d3879427e0ab842fb8f
modu-robotics <modu.robotics@gmail.com>
2024-05-15 13:16:19 -0400

    ,e remap + multicursor

--- init.lua
+++ init.lua
@@ -164,7 +164,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>e', ':e %:h', { desc = '[E]dit the directory of the current file' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -293,6 +293,18 @@ require('lazy').setup({
       require('which-key').register({
         ['<leader>h'] = { 'Git [H]unk' },
       }, { mode = 'v' })
+    end,
+  },
+
+  { -- multi cursor
+    'mg979/vim-visual-multi',
+    branch = 'master',
+    event = 'VeryLazy',
+    config = function()
+      vim.cmd [[
+    nmap <M-C-j> <Plug>(VM-Add-Cursor-Down)
+    nmap <M-C-k> <Plug>(VM-Add-Cursor-Up)
+    ]]
     end,
   },
 
